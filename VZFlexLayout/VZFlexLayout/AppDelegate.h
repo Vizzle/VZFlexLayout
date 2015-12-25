2abdd9676ef8f2478d2274c8294f747789bda8d7
vizlabxt <jayson.xu@foxmail.com>
2015-12-25 13:45:55 +0800

    update

---
+++ VZFlexLayout/VZFlexLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VZFlexLayout
+//
+//  Created by moxin on 15/12/25.
+//  Copyright © 2015年 Vizlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VZFlexLayout/VZFlexLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VZFlexLayout
+//
+//  Created by moxin on 15/12/25.
+//  Copyright © 2015年 Vizlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
